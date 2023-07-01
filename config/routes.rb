a299d9e3fc27cd344264b35a18c69a34cfcf8c90
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-01 14:46:36 +0900

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :blogs 
   root 'blogs#index'
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  # if Rails.env.development?
+  #   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  # end
 end
